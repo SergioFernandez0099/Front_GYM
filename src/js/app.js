65065d6a698e94976f4458482801a1268306f35b
SergioFernandez0099 <dialga.cuenco@gmail.com>
2025-11-07 03:26:33 +0100

    Implementación de footer y arreglo de grid en ejercicios

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,41 @@
-import { removeLoginCss } from "../utils/helpers.js";
-import { Navbar } from "./components/navbar.js";
-import { Login } from "./pages/login.js";
-import { initRouter, router } from "./router.js";
-import { setLoginStatus, getLoginStatus } from "./store.js";
+import {removeLoginCss} from "../utils/helpers.js";
+import {Navbar} from "./components/navbar.js";
+import {Login} from "./pages/login.js";
+import {initRouter, router} from "./router.js";
+import {getLoginStatus, setLoginStatus} from "./store.js";
+import {Footer} from "./components/footer.js";
 
 function renderApp() {
-  if (!getLoginStatus()) {
-    // Mostrar login primero
-    const app = document.getElementById("app");
-    app.innerHTML = "";
-    app.appendChild(
-      Login({
-        onLogin: () => {
-          setLoginStatus(true);
-          initSPA(); 
-          router.navigate("/");
-        },
-      })
-    );
-  } else {
-    initSPA();
-  }
+    if (!getLoginStatus()) {
+        // Mostrar login primero
+        const app = document.getElementById("app");
+        app.innerHTML = "";
+        app.appendChild(
+            Login({
+                onLogin: () => {
+                    setLoginStatus(true);
+                    initSPA();
+                    router.navigate("/");
+                },
+            })
+        );
+    } else {
+        initSPA();
+    }
 }
 
 function initSPA() {
-  removeLoginCss();
-  const navbarContainer = document.getElementById("navbar");
-  navbarContainer.innerHTML = "";
-  navbarContainer.appendChild(Navbar());
+    removeLoginCss();
+    const navbarContainer = document.getElementById("navbar");
+    const footerContainer = document.getElementById("footer");
 
-  // footer
-  
-  initRouter();
+    navbarContainer.innerHTML = "";
+    footerContainer.innerHTML = "";
+
+    navbarContainer.appendChild(Navbar());
+    footerContainer.appendChild(Footer());
+
+    initRouter();
 }
 
 // Iniciar aplicación
