7bd2ac8862f0e6822b729773da87ef8c9284aae5
SergioFernandez0099 <dialga.cuenco@gmail.com>
2025-12-19 02:18:52 +0100

    Borrado de archivos y dependencias no necesarios

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import {initRouter, loadNavbarAndFooter} from "./router.js";
 import {checkAndSetLogin} from "./store.js";
-import { provideFluentDesignSystem, fluentTooltip, fluentButton } from "@fluentui/web-components";
 
 import '../styles/index.css';
 
@@ -10,15 +9,8 @@ async function initSPA() {
         loadNavbarAndFooter();
     }
 
-
     initRouter();
 }
-
-
-provideFluentDesignSystem().register(
-    fluentTooltip(),
-    fluentButton()
-);
 
 // Iniciar aplicación
 await initSPA();
